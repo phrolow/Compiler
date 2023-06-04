563ccf1709ff3dbeca563f82bfb2e4d285d4d2db
phrolow <sheludcko.bogdan@gmail.com>
2023-06-04 13:05:13 +0300

    Fixed output. Wanna movr to floats

--- src/Libs/decimal.asm
+++ src/Libs/decimal.asm
@@ -12,7 +12,7 @@ BITS 64
 decimal push rbp
         mov rbp, rsp                ; stack frame
         
-        mov eax, [rbp + 16]    ; arg from printf call
+        mov eax, [rbp + 16]         ; arg from printf call
 
         mov r9d, 0x80000000         ; bit mask for 1st bit
         and r9d, eax                ; get sign
@@ -27,31 +27,29 @@ decimal push rbp
 
         mov rsi, 0x400800           ; output buffer
 
-        mov [rsi + rcx + 1], byte 0xa
+        mov [rsi + rcx + 1], byte 0xa ; \n in the end of buf
 
 .Next:  xor rdx, rdx
-
         mov rdi, 10
-
-        div rdi
+        div rdi                     ; div num by 10
 
         add dl, '0'                 ; to char
         mov byte [rsi + rcx], dl    ; bit to buffer
 
         cmp eax, 0
-        je .BreakLoop                   ; if our num lesser than r9d, break loop
+        je .BreakLoop               ; if our num lesser than r9d, break loop
 
         loop .Next                      
 .BreakLoop:
         mov rax, 1                  ; 1st syscall - write()
         add rsi, rcx                ; start output from last non-zero digit
-        mov rdx, 23                 ; length of write()
+        mov rdx, 20                 ; length of write()
         sub rdx, rcx                ; length of output
 
         cmp r9d, 1
         jne .skip_minus
 
-        dec rsi
+        dec rsi                     ; buf--
         inc rdx
         mov byte [rsi], '-'
 
--- src/Libs/in.asm
+++ src/Libs/in.asm
@@ -11,30 +11,30 @@ BITS 64
 
 _in     push rbp
         
-        xor rax, rax
-        xor rdi, rdi
-        mov rsi, 0x400820
-        mov rdx, 0x20
+        xor rax, rax            ; syscall 0 (read)
+        xor rdi, rdi            ; stdin
+        mov rsi, 0x400820       ; buf VA
+        mov rdx, 0x20           ; buf length
 
         syscall
 
-        mov rsi, 0x400820
-        xor rax, rax
-        mov r8, 10
-        xor rcx, rcx
+        mov rsi, 0x400820       ; buf VA
+        xor rax, rax            ; rax is result
+        mov r8, 10              ; multiplier
+        xor rcx, rcx            ; I'll store chars here
 
 .Convertation:
-        mul r8
+        mul r8                  
 
         mov cl, byte [rsi]
 
-        add rax, rcx
+        add rax, rcx   
 
-        sub rax, '0'
+        sub rax, '0'            
 
-        inc rsi
+        inc rsi                 ; rax = 10 * rax + (*(rsi++) - '0')
 
-        cmp [rsi], byte 10
+        cmp [rsi], byte 0xa     ; while not '\n'
         ja .Convertation
 
         pop rbp
